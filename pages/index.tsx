8b87ee208225623ca595852774ab340ee54b2de4
Vince Trapani <vmtrapani1@gmail.com>
2025-07-30 19:13:51 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import Head from 'next/head';
 import Hero from '@/components/Hero';
-import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>BuildChatt | Land For Sale in Chattanooga, TN</title>
-        <meta name="description" content="Premium land listing in Chattanooga, TN" />
+        <title>BuildChatt</title>
       </Head>
-      <main className="relative min-h-screen overflow-hidden">
-        <Header />
+      <main className="relative w-full min-h-screen overflow-hidden">
         <Hero />
       </main>
     </>
