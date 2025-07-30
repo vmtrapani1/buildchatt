359aa9eeef914b5615ba1470e80f33f9355f95ce
Vince Trapani <vmtrapani1@gmail.com>
2025-07-30 13:31:37 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>BuildChatt | Land For Sale in Chattanooga</title>
+        <title>BuildChatt | Land For Sale in Chattanooga, TN</title>
         <meta name="description" content="Premium land listing in Chattanooga, TN" />
       </Head>
       <main className="relative min-h-screen overflow-hidden">
