4411d3f3f4d268ee58cde6d94965650b57ffb383
Vince Trapani <vmtrapani1@gmail.com>
2025-07-30 09:53:15 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
     <header className={`fixed top-0 w-full z-50 transition-all duration-300 ${scrolled ? 'bg-black/80 h-16' : 'bg-transparent h-20'}`}>
       <div className="max-w-6xl mx-auto px-4 flex justify-between items-center h-full">
         {/* <img src="/logo.png" alt="BuildChatt" className={`transition-all ${scrolled ? 'h-10' : 'h-12'}`} /> */}
-        <div className="text-white hidden sm:block">Call: (423) 227-2924</div>
+           {/* <div className="text-white hidden sm:block">Call: (423) 227-2924</div> */}
       </div>
     </header>
   );
